e5738255ef6de278673d1bcc8a951a25b444499a
Henrique Machado <henriquefur@gmail.com>
2020-03-20 23:20:08 +0100

    Adds collider component.

--- src/components.rs
+++ src/components.rs
@@ -4,13 +4,26 @@ use specs::{Component, DenseVecStorage};
 #[derive(Component)]
 pub struct Transform {
     pub position: Vector2,
-    pub rotation: f32,
+    pub rotation: f64,
 }
 
 #[derive(Component)]
 pub struct RigidBody {
     pub velocity: Vector2,
     pub spin: f64,
+}
+
+#[derive(Component)]
+pub struct Collider {
+    pub collider_type: ColliderType,
+    pub radius: f64,
+    pub size: Vector2,
+    pub is_trigger: bool,
+}
+
+pub enum ColliderType {
+    Sphere,
+    Rectangle,
 }
 
 #[derive(Component)]
--- src/physics_system.rs
+++ src/physics_system.rs
@@ -4,10 +4,14 @@ use specs::*;
 pub struct PhysicsSystem;
 
 impl<'a> System<'a> for PhysicsSystem {
-    type SystemData = (ReadStorage<'a, RigidBody>, WriteStorage<'a, Transform>);
+    type SystemData = (
+        ReadStorage<'a, RigidBody>,
+        ReadStorage<'a, Collider>,
+        WriteStorage<'a, Transform>,
+    );
 
-    fn run(&mut self, (rigid_bodies, mut transforms): Self::SystemData) {
-        for (rigid_body, transform) in (&rigid_bodies, &mut transforms).join() {
+    fn run(&mut self, (rigid_bodies, colliders, mut transforms): Self::SystemData) {
+        for (rigid_body, collider, transform) in (&rigid_bodies, &colliders, &mut transforms).join() {
             transform.position += rigid_body.velocity;
         }
     }
