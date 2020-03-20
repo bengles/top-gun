be3aadf2cf94719335849de990a76d32bcbf786b
bengles <benglesson@gmail.com>
2020-03-21 00:29:12 +0100

    Working on player action system

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,13 @@ mod assets;
 mod components;
 mod input;
 mod input_to_player_action_system;
+mod player_action_system;
 
 use assets::*;
 use components::*;
 use input::*;
 use input_to_player_action_system::*;
+use player_action_system::*;
 
 // Define usual 2d data structs.
 pub type Point2 = ggez::nalgebra::Point2<f32>;
