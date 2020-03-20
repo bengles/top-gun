accf8ab0dbfb6fd930300e7380d03d6e4cbb1243
bengles <benglesson@gmail.com>
2020-03-20 17:33:30 +0100

    Add template GGEZ main.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use ggez::{graphics, Context, ContextBuilder, GameResult};
+use ggez::event::{self, EventHandler};
+
 fn main() {
-    println!("Hello, world!");
+    // Make a Context.
+    let (mut ctx, mut event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
+		.build()
+		.expect("aieee, could not create ggez context!");
+
+    // Create an instance of your event handler.
+    // Usually, you should provide it with the Context object to
+    // use when setting your game up.
+    let mut my_game = TopGun::new(&mut ctx);
+
+    // Run!
+    match event::run(&mut ctx, &mut event_loop, &mut my_game) {
+        Ok(_) => println!("Exited cleanly."),
+        Err(e) => println!("Error occured: {}", e)
+    }
+}
+
+struct TopGun {
+    // Your state here...
+}
+
+impl TopGun {
+    pub fn new(_ctx: &mut Context) -> TopGun {
+        // Load/create resources such as images here.
+        TopGun {
+            // ...
+        }
+    }
+}
+
+impl EventHandler for TopGun {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // Update code here...
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::BLACK);
+        // Draw code here...
+        graphics::present(ctx)
+    }
 }
